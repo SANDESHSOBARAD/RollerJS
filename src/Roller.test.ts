ac8f525cec64af3d577f6b28b4538748b4bf73eb
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2023-03-30 11:29:23 -0700

    test cases for Roller

--- src/Roller.test.ts
+++ src/Roller.test.ts
@@ -7,6 +7,95 @@ describe("Smoke test", ()=> {
 })
 
 describe("Roller tests", ()=> {
-  test("Description", () => {
+  describe('constructor', () => {
+    test('should create a Roller with 2 faces if no value is provided', () => {
+      const roller = new Roller(1);
+      expect(roller instanceof Roller).toBe(true);
+      expect(roller['_faces']).toBe(2);
+    });
+
+    test('should create a Roller with the specified number of faces if it is between 2 and 20', () => {
+      const roller = new Roller(10);
+      expect(roller instanceof Roller).toBe(true);
+      expect(roller['_faces']).toBe(10);
+    });
+
+    test('should create a Roller with 2 faces if the specified number of faces is less than 2', () => {
+      const roller = new Roller(-10);
+      expect(roller instanceof Roller).toBe(true);
+      expect(roller['_faces']).toBe(2);
+    });
+
+    test('should create a Roller with 20 faces if the specified number of faces is greater than 20', () => {
+      const roller = new Roller(30);
+      expect(roller instanceof Roller).toBe(true);
+      expect(roller['_faces']).toBe(20);
+    });
+
+    test('should truncate any non-integer values of faces to an integer', () => {
+      const roller1 = new Roller(3.5);
+      const roller2 = new Roller(7);
+      const roller3 = new Roller(null);
+      expect(roller1['_faces']).toBe(3);
+      expect(roller2['_faces']).toBe(7);
+      expect(roller3['_faces']).toBe(2);
+    });
+  });
+
+  test('should return the value of the rolled number and update the distribution for a valid roll', () => {
+    const roller = new Roller(6);
+    const value = roller.roll(4);
+    expect(value).toBe(4);
+    expect(roller['_last']).toBe(4);
+    expect(roller['_distribution'].get(4)).toBe(1);
+  });
+
+  test('distribution() should return a Map with correct keys and values after rolls', () => {
+    const roller = new Roller(6);
+    roller.roll(1);
+    roller.roll(2);
+    roller.roll(3);
+    roller.roll(3);
+    roller.roll(4);
+    roller.roll(4);
+    roller.roll(4);
+    roller.roll(5);
+    roller.roll(6);
+    expect(roller.distribution().size).toBe(6);
+    expect(roller.distribution().get(1)).toBe(1);
+    expect(roller.distribution().get(2)).toBe(1);
+    expect(roller.distribution().get(3)).toBe(2);
+    expect(roller.distribution().get(4)).toBe(3);
+    expect(roller.distribution().get(5)).toBe(1);
+    expect(roller.distribution().get(6)).toBe(1);
+  });
+
+  test('distribution() should return a Map with correct keys and values after no rolls', () => {
+    const roller = new Roller(6);
+    expect(roller.distribution().size).toBe(6);
+    expect(roller.distribution().get(1)).toBe(0);
+    expect(roller.distribution().get(2)).toBe(0);
+    expect(roller.distribution().get(3)).toBe(0);
+    expect(roller.distribution().get(4)).toBe(0);
+    expect(roller.distribution().get(5)).toBe(0);
+    expect(roller.distribution().get(6)).toBe(0);
+  });
+  
+  test('last() should return the correct value after rolls are made', () => {
+    const roller = new Roller(6);
+    roller.roll(1);
+    roller.roll(2);
+    expect(roller.last()).toBe(2);
+    roller.roll(3);
+    expect(roller.last()).toBe(3);
+    roller.roll(3);
+    expect(roller.last()).toBe(3);
+    roller.roll(4);
+    roller.roll(4);
+    roller.roll(4);
+    expect(roller.last()).toBe(4);
+    roller.roll(5);
+    roller.roll(6);
+    expect(roller.last()).toBe(6);
   });
 });
--- src/Roller.ts
+++ src/Roller.ts
@@ -8,7 +8,13 @@ export class Roller {
     // the common coin flip. All numbers in this class should be truncated as
     // an integer if any non-integer number is provided.
     constructor(faces: number) {
-        faces;
+        //to ensure if the value is in the valid range.
+        this._faces = Math.max(2, Math.min(20, Math.trunc(faces)));
+        
+        this._distribution = new Map<number, number>();
+        for (let i=1; i<=this._faces; i++){
+            this._distribution.set(i,0);
+        }
     }
 
     // specifies which number should be rolled and returns the recorded roll. 
@@ -22,13 +28,24 @@ export class Roller {
     //      key (value rolled) has an updated value that increases the number
     //      of rolls for that face by 1
     roll(value: number): number{
-        return value;
+        //to ensure the value is within the valid range
+        value = value < 1 ? 1 : value;
+        value = value > this._faces ? this._faces : value;
+        const rVal = Math.trunc(value);
+
+        //to update the distribution map and last roll value
+        if(rVal >= 1 && rVal <= this._faces){
+            this._distribution.set(rVal, this._distribution.get(rVal)! + 1);
+            this._last = rVal;
+        }
+
+        return this._last;
     }
 
     // returns the value of the latest die roll. If no rolls have been made
     // yet, then it should return 0.
     last(): number {
-        return 0;
+        return this._last;
     }
 
     // returns a Map that represents the current distribution of rolls for each
@@ -37,6 +54,6 @@ export class Roller {
     // rolled. Even if a face has not yet been rolled, its key should have a
     // value of 0.
     distribution(): Map<number, number>{
-        return new Map<number, number>();
+        return new Map<number, number>(this._distribution);
     }
 } // end of Roller class
